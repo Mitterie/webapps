2ba2a03f9beb9cb89f70644964bcaccf5c767b3f
Victor Dhorme <pipodhorme@gmail.com>
2023-12-06 23:16:36 +0100

    main update psql

--- Mitterie/WEB-INF/classes/Main.java
+++ Mitterie/WEB-INF/classes/Main.java
@@ -1,4 +1,9 @@
+import java.sql.Statement;
 import java.io.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.util.Properties;
 
 import jakarta.servlet.*;
 import jakarta.servlet.http.*;
@@ -17,10 +22,39 @@ public class Main extends HttpServlet
             res.setContentType("text/html");
             PrintWriter out = res.getWriter();
             out.println("<link rel=\"stylesheet\" href=\'./css/main.css\'>");
-            out.println("<body><h1>Connection établie !</h1></body>");
+            out.println("<body><h1>Vidéos des mites :</h1><table>");
+            out.println(getAllVideosHtml()+"</table></body>");
         }else{
             res.sendRedirect("http://51.91.101.98:8080/Mitterie/");
         }
+    }
+
+    public static String getAllVideosHtml(){
+        String content = "";
+        try{
+            Properties pr = new Properties();
+            pr.load(new FileInputStream("/opt/tomcat/postgres.prop"));
+            // enregistrement du driver
+            Class.forName(pr.getProperty("driver"));
+            // connexion à la base
+            String url = pr.getProperty("url");
+            String nom = pr.getProperty("nom");
+            String mdp = pr.getProperty("mdp");
+            Connection con = DriverManager.getConnection(url, nom, mdp);
+            try{
+                Statement stmt = con.createStatement();
+                ResultSet rs = stmt.executeQuery("SELECT * FROM videosmitterie;");
+                while(rs.next()){
+                    content = content + "<tr><td><iframe width=\"480\" height=\"270\" src=\"https://www.youtube.com/embed/"+rs.getString(1)+"\" allow=\"accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share\" allowfullscreen></iframe></td><td>"+rs.getString(2)+"</td></tr>";
+                }
+            }catch(Exception e2){
+                System.out.println(e2.getMessage());
+                con.close();
+            }
+        }catch(Exception e1){
+            System.out.println(e1.getMessage());
+        }
+        return content;
     }
 
 }
