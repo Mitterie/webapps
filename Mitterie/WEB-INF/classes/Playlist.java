793d2974eccdd973c86e7afac9144d47d056ad2b
Victor Dhorme <pipodhorme@gmail.com>
2023-12-12 18:00:32 +0100

    add retour & disconnect playlist

--- Mitterie/WEB-INF/classes/Playlist.java
+++ Mitterie/WEB-INF/classes/Playlist.java
@@ -16,7 +16,7 @@ public class Playlist extends HttpServlet
             PrintWriter out = res.getWriter();
             out.println("<link rel=\"stylesheet\" href=\'./css/playlist.css\'>");
             out.println("<title>Playlist Officiel</title>");
-            out.println("<body><h1>Régale toi chef !</h1><div class=\"carBT\">");
+            out.println("<body><h1>Régale toi chef !</h1><div class=\"carBT\"><a href=\"Disconnect\">Se déconnecter</a><br><a href=\"Entrance\">Retour</a>");
             out.println("<iframe style=\"border-radius:12px\" src=\"https://open.spotify.com/embed/playlist/3EcbQmX1J0E4Ii8uty41VS?utm_source=generator&theme=0\" width=\"500\" height=\"352\" frameBorder=\"0\" allowfullscreen=\"\" allow=\"autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture\" loading=\"lazy\"></iframe>");
             out.println("</body>");
         }else{
