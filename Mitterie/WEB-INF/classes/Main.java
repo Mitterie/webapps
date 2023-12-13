bfeb4474fd1e8ce73a014c3ae0ad53afe2e49465
Victor Dhorme <pipodhorme@gmail.com>
2023-12-13 21:51:53 +0100

    essai 3 main Croissant

--- Mitterie/WEB-INF/classes/Main.java
+++ Mitterie/WEB-INF/classes/Main.java
@@ -26,15 +26,24 @@ public class Main extends HttpServlet
             }
 
             boolean croissant = false;
-            if(req.getParameter("jeune") != null){
-                croissant = true;
+            String affichageCroissant = "";
+            if(session.getAttribute("MainCroissant") != null){
+                if((boolean)session.getAttribute("MainCroissant")){
+                    croissant = true;
+                    affichageCroissant = "checked";
+                }else{
+                    croissant = false;
+                }
+            }else{
+                croissant = false;
             }
+
             res.setContentType("text/html");
             PrintWriter out = res.getWriter();
             out.println("<link rel=\"stylesheet\" href=\'./css/main.css\'>");
             out.println("<title>Videos</title>");
             out.println("<div class=\"list\"><h1>On fait quoi ?</h1><ul><li><a href=\"Entrance\">Retour</a></li><li><form action=Main method=post><input name=rech type=text placeholder=\"Rechercher...\"><input type=submit value=\"Valider\"></form></li><li><a href=\"Disconnect\">Se déconnecter</a></li></div>");
-            out.println("<body><div class=\"videos\"><h2>Vidéos des mites</h2><form action=\"Main\" method=\"post\"><input type=\"checkbox\" name=\"jeune\" value=\"vrai\"/><label for=\"jeune\">Ordre croissant</label><button type=\"submit\">Recharger</button></form>");
+            out.println("<body><div class=\"videos\"><h2>Vidéos des mites</h2><form action=\"SaveCroissantMain\" method=\"post\"><input type=\"checkbox\" name=\"jeune\" value=\"vrai\" "+affichageCroissant+"/><label for=\"jeune\">Ordre croissant</label><button type=\"submit\">Recharger</button></form>");
             out.println(getAllVideosHtml(croissant,s)+"</div></body>");
         }else{
             res.sendRedirect("http://51.91.101.98:8080/Mitterie/");
---
+++ Mitterie/WEB-INF/classes/SaveCroissantMain.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.WebServlet;
+
+@WebServlet(name = "SaveCroissantMain", urlPatterns = {"/SaveCroissantMain"})
+
+public class SaveCroissantMain extends HttpServlet
+{
+    public void doPosts( HttpServletRequest req, HttpServletResponse res )
+    throws ServletException, IOException
+    {
+        HttpSession session = req.getSession();
+        if(session.getAttribute("token") != null){
+            if(req.getParameter("jeune") != null){
+                session.setAttribute("MainCroissant", true);
+            }else{
+                session.setAttribute("MainCroissant", false);
+            }
+        }
+        res.sendRedirect("Main");
+    }
+}
