c22298875b1b2d56d9b4b89cc7fa1c84df7c26cb
Victor Dhorme <pipodhorme@gmail.com>
2023-12-14 19:39:23 +0100

    save du role sur la session de l'utilisateur

--- Mitterie/WEB-INF/classes/Authent.java
+++ Mitterie/WEB-INF/classes/Authent.java
@@ -9,21 +9,19 @@ import jakarta.servlet.*;
 import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.WebServlet;
 
-@WebServlet(name = "Authent", urlPatterns = {"/Authent"})
+@WebServlet(name = "Authent", urlPatterns = { "/Authent" })
 
-public class Authent extends HttpServlet
-{
-    public void doPost( HttpServletRequest req, HttpServletResponse res )
-    throws ServletException, IOException
-    {
+public class Authent extends HttpServlet {
+    public void doPost(HttpServletRequest req, HttpServletResponse res)
+            throws ServletException, IOException {
 
         boolean valid = false;
         String l = req.getParameter("uname");
         String m = req.getParameter("upswd");
 
-        if(l != null && m != null){
-            if(!l.equals("") && !m.equals("")){
-                try{
+        if (l != null && m != null) {
+            if (!l.equals("") && !m.equals("")) {
+                try {
                     Properties pr = new Properties();
                     pr.load(new FileInputStream("/opt/tomcat/postgres.prop"));
                     // enregistrement du driver
@@ -33,35 +31,70 @@ public class Authent extends HttpServlet
                     String nom = pr.getProperty("nom");
                     String mdp = pr.getProperty("mdp");
                     Connection con = DriverManager.getConnection(url, nom, mdp);
-                    try{
+                    try {
                         // exécution de la requete
-                        PreparedStatement pstmt = con.prepareStatement("SELECT * FROM loginmitterie WHERE login = ? AND mdp = MD5(?);");
+                        PreparedStatement pstmt = con
+                                .prepareStatement("SELECT * FROM loginmitterie WHERE login = ? AND mdp = MD5(?);");
                         pstmt.setString(1, l);
                         pstmt.setString(2, m);
                         ResultSet rs = pstmt.executeQuery();
-                        if(rs.next()){
+                        if (rs.next()) {
                             valid = true;
                         }
                         con.close();
-                    }catch(Exception e2){
+                    } catch (Exception e2) {
                         System.out.println(e2.getMessage());
                         con.close();
                     }
-                }catch(Exception e1){
+                } catch (Exception e1) {
                     System.out.println(e1.getMessage());
                 }
             }
         }
 
-        if(!valid){
+        if (!valid) {
             res.sendRedirect("http://51.91.101.98:8080/Mitterie/");
-        }else{
+        } else {
             HttpSession session = req.getSession(true);
             String token = l;
             session.setAttribute("token", token);
+            session.setAttribute("role", getRole(l));
             session.setMaxInactiveInterval(3600);
             res.sendRedirect("Entrance");
         }
+    }
+
+    public static String getRole(String login) {
+        String res = "";
+        try {
+            Properties pr = new Properties();
+            pr.load(new FileInputStream("/opt/tomcat/postgres.prop"));
+            // enregistrement du driver
+            Class.forName(pr.getProperty("driver"));
+            // connexion à la base
+            String url = pr.getProperty("url");
+            String nom = pr.getProperty("nom");
+            String mdp = pr.getProperty("mdp");
+            Connection con = DriverManager.getConnection(url, nom, mdp);
+            try {
+                // exécution de la requete
+                PreparedStatement pstmt = con.prepareStatement("SELECT role FROM loginmitterie WHERE login = ?");
+                pstmt.setString(1, login);
+                ResultSet rs = pstmt.executeQuery();
+                while (rs.next()) {
+                    res = rs.getString(1);
+                }
+                con.close();
+            } catch (Exception e2) {
+                System.out.println(e2.getMessage());
+                res = "utils";
+                con.close();
+            }
+        } catch (Exception e1) {
+            System.out.println(e1.getMessage());
+            res = "utils";
+        }
+        return res;
     }
 
 }
