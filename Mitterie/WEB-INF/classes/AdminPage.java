cc7dc12ec98ee3dd014d44a56c5877a9da566111
Victor Dhorme <pipodhorme@gmail.com>
2024-01-10 22:21:13 +0100

    siu

--- Mitterie/WEB-INF/classes/AdminPage.java
+++ Mitterie/WEB-INF/classes/AdminPage.java
@@ -52,7 +52,7 @@ public class AdminPage extends HttpServlet {
                 Statement stmt = con.createStatement();
                 ResultSet rs = stmt.executeQuery("SELECT id,login,role,email,daylastco,hourlastco FROM loginmitterie;");
                 while(rs.next()){
-                    res = res +"<tr><td>"+rs.getString(1)+"</td><td>"+rs.getString(2)+"</td><td>"+rs.getString(3)+"</td><td>"+rs.getString(4)+"</td><td>"+rs.getString(6)+"</td><td>\"+rs.getString(3)+\"</td></tr>";
+                    res = res +"<tr><td>"+rs.getString(1)+"</td><td>"+rs.getString(2)+"</td><td>"+rs.getString(3)+"</td><td>"+rs.getString(4)+"</td><td>"+rs.getString(5)+"</td><td>"+rs.getString(6)+"</td></tr>";
                 }
                 con.close();
             } catch (Exception e2) {
