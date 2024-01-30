c12f84bb2d44c3ac76951a9fb294512bd5800e1c
Victor Dhorme <pipodhorme@gmail.com>
2024-01-30 17:01:08 +0100

    siu

--- Mitterie/WEB-INF/classes/UploadVideo.java
+++ Mitterie/WEB-INF/classes/UploadVideo.java
@@ -2,6 +2,7 @@ import java.io.*;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.Statement;
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.Properties;
@@ -13,21 +14,20 @@ import jakarta.servlet.annotation.WebServlet;
 @WebServlet(name = "UploadVideo", urlPatterns = { "/UploadVideo" })
 
 public class UploadVideo extends HttpServlet {
-    public void doPost( HttpServletRequest req, HttpServletResponse res )
-    throws ServletException, IOException
-    {
+    public void doPost(HttpServletRequest req, HttpServletResponse res)
+            throws ServletException, IOException {
         HttpSession session = req.getSession();
         if (session.getAttribute("token") != null) {
             if (session.getAttribute("role") != null) {
                 if (((String) session.getAttribute("role")).equals("admin")) {
                     String urlVideo = req.getParameter("url");
                     String titreVideo = req.getParameter("titre");
-                    if(urlVideo != null && titreVideo != null){
-                        if(!urlVideo.equals("") &&  !titreVideo.equals("")){
+                    if (urlVideo != null && titreVideo != null) {
+                        if (!urlVideo.equals("") && !titreVideo.equals("")) {
                             boolean fait = false;
-                            try{
+                            try {
                                 String[] urlTab = urlVideo.split("/");
-                                try{
+                                try {
                                     Properties pr = new Properties();
                                     pr.load(new FileInputStream("/opt/tomcat/postgres.prop"));
                                     // enregistrement du driver
@@ -37,32 +37,35 @@ public class UploadVideo extends HttpServlet {
                                     String nom = pr.getProperty("nom");
                                     String mdp = pr.getProperty("mdp");
                                     Connection con = DriverManager.getConnection(url, nom, mdp);
-                                    try{
-                                        PreparedStatement stmt = con.prepareStatement("INSERT INTO videosmitterie VALUES (?,?,'"+LocalDate.now().toString()+"','"+LocalTime.now().toString()+"');");
+                                    try {
+                                        PreparedStatement stmt = con.prepareStatement(
+                                                "INSERT INTO videosmitterie VALUES (?,?,'" + LocalDate.now().toString()
+                                                        + "','" + LocalTime.now().toString() + "');");
                                         stmt.setString(1, urlTab[3]);
                                         stmt.setString(2, titreVideo);
                                         int r = stmt.executeUpdate();
-                                        if(r != -1){
-                                            fait = true; 
-                                            PreparedStatement stmt2 = con.prepareStatement("NOTIFY bot, ?");
-                                            stmt2.setString(1, titreVideo);
-                                            stmt2.execute();
+                                        if (r != -1) {
+                                            fait = true;
+                                            Statement stmt2 = con.createStatement();
+                                            stmt2.executeUpdate("NOTIFY bot, '" + titreVideo + "';");
                                         }
                                         con.close();
-                                    }catch(Exception e2){
+                                    } catch (Exception e2) {
                                         con.close();
                                     }
-                                }catch(Exception e1){}
-                            }catch(Exception e){} 
-                            if(fait){
+                                } catch (Exception e1) {
+                                }
+                            } catch (Exception e) {
+                            }
+                            if (fait) {
                                 res.sendRedirect("Entrance");
-                            }else{
+                            } else {
                                 res.sendRedirect("UploadPage");
                             }
-                        }else{
+                        } else {
                             res.sendRedirect("UploadPage");
                         }
-                    }else{
+                    } else {
                         res.sendRedirect("UploadPage");
                     }
 
